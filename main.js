2dec29a76c98c4d20114dfed41bad3b4b1837e76
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-23 16:44:51 +0200

    modifica HTML
    modificato HTML e js per stampare in pagina

--- main.js
+++ main.js
@@ -13,21 +13,31 @@ const unitCost = 0.21;
 // Prezzo biglietto
 let price = unitCost * distance;
 
-console.log('Il prezzo base è ', String(price), 'euro');
+//console.log('Il prezzo base è ', String(price), 'euro');
 
+let discount;
 // Applicazione sconti
 if (age < 18) {
     price = price * 0.8;
     // Arrontondamento a due cifre decimali
     price = Math.round(price*100)/100;
     console.log('Il prezzo con sconto minorenni è ', String(price), 'euro');
+    discount = 'sconto minorenni';
 } else if (age > 65) {
     price = price * 0.6;
     // Arrontondamento a due cifre decimali
     price = Math.round(price*100)/100;
     console.log('Il prezzo con sconto over 65 è ', String(price), 'euro');
+    discount = 'sconto over 65';
 } else {
     // Arrontondamento a due cifre decimali
     price = Math.round(price*100)/100;
     console.log('Il prezzo è ', String(price),"euro, non sono previsti sconti per questa fascia d'età");
+    discount = "non sono previsti sconti per questa fascia d'età";
 }
+
+// Stampa in pagina
+document.getElementById('distance').innerHTML = distance;
+document.getElementById('age').innerHTML = age;
+document.getElementById('price').innerHTML = price;
+document.getElementById('discount').innerHTML = discount;
